b9fcaabbc20a32c64d0df7e178ff420095e99b3a
Ma100Dev <33522543+Ma100Dev@users.noreply.github.com>
2022-07-29 19:26:51 +0300

    12.11

--- todo-app/todo-backend/routes/index.js
+++ todo-app/todo-backend/routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
+const redis = require('../redis')
 const configs = require('../util/config')
 
 let visits = 0
@@ -13,6 +14,14 @@ router.get('/', async (req, res) => {
     ...configs,
     visits
   });
+});
+
+router.get('/usage', async (_, res) => {
+  res.send(
+    {
+      "added_todos": Number(await redis.getAsync('added_todos')) || 0
+    }
+  );
 });
 
 module.exports = router;
